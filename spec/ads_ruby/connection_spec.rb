a60fad369c72604d3c2b37a6b14f785ce36f1d34
panjan <jan.zdrahal@gooddata.com>
2017-11-29 19:19:37 +0100

    Fix connection spec

--- spec/ads_ruby/connection_spec.rb
+++ spec/ads_ruby/connection_spec.rb
@@ -3,6 +3,6 @@ require_relative '../../lib/ads_ruby/connection'
 RSpec.describe AdsRuby::Connection do
   it 'authenticates' do
     result = subject.authenticate
-    expect(result).not_to be empty
+    expect(result).to be
   end
 end
